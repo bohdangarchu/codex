94651508c9939da14573a82da63a7ebb944fb5de
Bohdan Garchu <bohdan.garchu@gmail.com>
2022-12-13 23:25:07 +0100

    setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 docker build --tag js-code-runner ./execution-service/docker/js
 docker build --tag python-code-runner ./execution-service/docker/python
-docker start test-mongo
+docker run --name test-mongo -d -p 27017:27017 mongo
 docker run --name rmq -p 5672:5672 rabbitmq
