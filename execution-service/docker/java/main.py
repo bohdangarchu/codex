54b23df63564caa796c8e57445d946a8cfaca784
Bohdan Garchu <bohdan.garchu@gmail.com>
2022-12-22 15:41:38 +0100

    add java support (wip)

---
+++ execution-service/docker/java/main.py
@@ -0,0 +1,42 @@
+import subprocess
+import sys
+import json
+
+def write_code_to_file(code):
+   file = open('Main.java', 'x')
+   file.write(code)
+   file.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('no input was given')
+        exit()
+    code = sys.argv[1]
+    write_code_to_file(code)
+    timeout = 10
+    try:
+        res = subprocess.run(
+                ['javac', 'Main.java'], 
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.PIPE,
+                timeout=timeout
+            )
+        res2 = subprocess.run(
+                ['java', 'Main'], 
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.PIPE,
+                timeout=timeout
+            )
+        output = {
+            "stdout": res.stdout.decode('utf-8') + res2.stdout.decode('utf-8'),
+            "stderr": res.stderr.decode('utf-8') + res2.stderr.decode('utf-8'),
+            "timeout": False
+        }
+        print(json.dumps(output))
+    except subprocess.TimeoutExpired:
+        print(json.dumps({
+            "stdout": "",
+            "stderr": "",
+            "timeout": True
+        }))
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build --tag js-code-runner ./execution-service/docker/js
 docker build --tag python-code-runner ./execution-service/docker/python
+docker build --tag java-code-runner ./execution-service/docker/java
 docker run --name test-mongo -d -p 27017:27017 mongo
 docker run --name rmq -p 5672:5672 rabbitmq
